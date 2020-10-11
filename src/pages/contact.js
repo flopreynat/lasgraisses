e15693121fbe831ad1d0c5b516d20d96f6ce0521
flopreynat <flopreynat@protonmail.com>
2020-10-11 19:49:58 +0200

    contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,10 +7,38 @@ const ContactPage = () => {
     return (
         <Layout>
             <Head title="Contact"/>
-            <h1>Contact</h1>
-            <p>
-                The best way to reach me is via <a href="https://twitter.com/@mytwitterhandle" target="_blank">@myself</a> on Twitter!
-            </p>
+            <div class="container">
+                <div class="pageTitle">
+                <h1>Contact</h1>
+                </div>
+                <div class="row">
+                    <div class="col-md-5">
+                        <p>
+                            <h4>Adresse Postale</h4>
+                            <ul class="ulNoBullet">
+                                <li>MAIRIE</li>
+                                <li>12 Place Colonel Charles Du Pin</li>
+                                <li>81300 LASGRAÏSSES</li>    
+                            </ul>
+                            <h4>Téléphone et email</h4>
+                            <ul class="ulNoBullet">
+                                <li>05 63 33 00 69</li>
+                                <li>05 63 81 42 34</li>
+                                <li>mairie-de-lasgraisses@e-kiwi.fr</li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div class="col-md-7">
+                        <p>
+                            <h4>Horaires du Secrétariat de Mairie</h4>
+                            Du Lundi au Vendredi de 08h30 à 12h30
+                            <br/><br/>Monsieur le Maire reçoit le Mardi matin.
+                            <br/>Monsieur le 1er Adjoint reçoit le Vendredi matin.
+                            <br/>Tous les autres rendez-vous peuvent faire l'objet d'une demande par mail ou auprès du Secrétariat.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
